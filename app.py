7f686f5bd7d62284465bf6433817bd97807f4bb6
imisi99 <isongrichard234@gmail.com>
2025-01-21 22:47:12 +0100

    saving models

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ import numpy as np
 
 app = FastAPI()
 
-model = tf.keras.models.load_model('')
-app.post('/predict')
+model_path = ['model0.h5', 'model1.h5', 'model2.h5']
+models = [tf.keras.models.load_model(path) for path in model_path]
+
+
+@app.post('/predict')
 async def post_picture(picture: UploadFile = File(...)):
     try:
         image = Image.open(picture.file).convert("L")
@@ -15,8 +18,8 @@ async def post_picture(picture: UploadFile = File(...)):
         image_array = np.array(image) / 255.0
         image_array = image_array.reshape(1, 28, 28, 1)
 
-        prediction = model.predict(image_array)
-        prediction_digit = np.argmax(prediction)
+        prediction = [model.predict(image_array) for model in models]
+        prediction_digit = [np.argmax(pred) for pred in prediction]
 
         return {'digit': prediction_digit}
 
