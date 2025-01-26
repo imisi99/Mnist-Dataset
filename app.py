c093a72469cb217748ea0e5a2b22ea37a61b8f98
imisi99 <isongrichard234@gmail.com>
2025-01-26 22:28:12 +0100

    modifying api

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 import tensorflow as tf
 import numpy as np
+import cv2
 
 app = FastAPI()
 
@@ -13,15 +14,25 @@ models = [tf.keras.models.load_model(path) for path in model_path]
 async def post_picture(picture: UploadFile = File(...)):
     try:
         image = Image.open(picture.file).convert("L")
-        image = image.resize((28, 28))
+        processed_image = process_image(image)
 
-        image_array = np.array(image) / 255.0
-        image_array = image_array.reshape(1, 28, 28, 1)
-
-        prediction = [model.predict(image_array) for model in models]
+        prediction = [model.predict(processed_image) for model in models]
         prediction_digit = [int(np.argmax(pred)) for pred in prediction]
 
         return {'digit': prediction_digit}
 
     except Exception as e:
         return {'error': str(e)}
+
+
+def process_image(image_array):
+    image_array = np.array(image_array)
+
+    image_array = cv2.GaussianBlur(image_array, (5, 5), 0)
+    _, image_array = cv2.threshold(image_array, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    image_array = cv2.resize(image_array, (28, 28))
+    image_array = image_array / 255.0
+    image_array = image_array.reshape(1, 28, 28, 1)
+
+    return image_array
